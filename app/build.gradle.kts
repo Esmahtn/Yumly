6c22696b8a5a6a7f7fad5582fec3e5b475eb2ffd
Esma Hatun Akbulut <153107265+Esmahtn@users.noreply.github.com>
2025-09-24 20:53:50 +0300

    gradle işlemleri

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
@@ -51,6 +55,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
